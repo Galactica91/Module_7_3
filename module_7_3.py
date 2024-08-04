3f86a7ba201ccb0d788ff81fa2191b22f405e6c0
Galactica91 <osheva91@yandex.com>
2024-08-04 11:50:01 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -5,7 +5,7 @@ class WordsFinder:
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
-            with open(file_name, 'r') as file:
+            with open(file_name, 'r', encoding='utf-8') as file:
                 text = file.read().lower()
                 for char in [',', '.', '=', '!', '?', ';', ':', ' - ']:
                     text = text.replace(char, ' ')
@@ -25,6 +25,7 @@ class WordsFinder:
         for name, words in self.get_all_words().items():
             result[name] = words.count(word.lower())
         return result
+
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words())

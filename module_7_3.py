820e37e283057cf6181e6ecc77e804abe335c731
Galactica91 <osheva91@yandex.com>
2024-08-04 10:23:15 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -26,8 +26,7 @@ class WordsFinder:
             result[name] = words.count(word.lower())
         return result
 
-# Пример выполнения программы:
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())  # Все слова
-print(finder2.find('TEXT'))    # 3 слово по счёту
-print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
